dba200f9c173b1fdeedef6a71f7bb4a550285f29
Philipp Pähler <philipp.paehler@gmx.net>
2021-04-17 11:14:14 +0200

    Remove scrolling calendar

--- src/pages/termin.tsx
+++ src/pages/termin.tsx
@@ -1,7 +1,9 @@
 import Head from "@docusaurus/Head";
 import Layout from "@theme/Layout";
+import clsx from "clsx";
 import React from "react";
 import { TerminHeaderComponent } from "../components/terminHeaderComponent";
+import styles from "./styles.module.css";
 
 function Termin() {
   return (
@@ -10,13 +12,13 @@ function Termin() {
         <script src="https://api.patient.doctena.de/js/widgetBooking/calendar/build.php"></script>
       </Head>
       <TerminHeaderComponent />
-
       <section
         data-toggle="doc-calendar"
         data-practice-eid={"65079ecc-2000-4ce5-a605-13ebd5f08427"}
         data-language="de"
-        style={{ minHeight: "600px" }}
+        className="iframe-container"
       />
+      <div className={clsx(styles.buttons)} />
     </Layout>
   );
 }
